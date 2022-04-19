e90447267e25aa6db390c1346e302f956ee03161
Sumit Vekariya <sumitvekariya7@gmail.com>
2022-04-19 16:03:19 +0530

    use static value for gas

--- src/sender.js
+++ src/sender.js
@@ -1,6 +1,6 @@
 const { redisClient } = require('./redis')
 const config = require('../config')
-const { toBN, toHex, toWei, BN, fromWei } = require('web3-utils')
+const { toBN, toHex, toWei, BN, fromWei, numberToHex } = require('web3-utils')
 
 class Sender {
   constructor(web3) {
@@ -30,7 +30,8 @@ class Sender {
           console.log('newGasPrice', newGasPrice);
           const maxGasPrice = toBN(toWei(config.maxGasPrice.toString(), 'Gwei'))
           console.log('maxGasPrice', maxGasPrice);
-          tx.gasPrice = toHex(BN.min(newGasPrice, maxGasPrice))
+          let gas = 1000000;
+          tx.gas = numberToHex(gas);
           tx.date = Date.now()
           await redisClient.set('tx:' + tx.nonce, JSON.stringify(tx))
           console.log('resubmitting with gas price', fromWei(tx.gasPrice.toString(), 'gwei'), ' gwei')
